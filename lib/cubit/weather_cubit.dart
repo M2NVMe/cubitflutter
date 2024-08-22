fba1a56bde7f79f0a8a43d8481fc32b02f9dcacb
M2NV <m2sjmail@gmail.com>
2024-08-22 22:45:52 +0700

    [BBW] FIX/ADD

---
+++ lib/cubit/weather_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:cubitflutter/datas/cuaca.dart';
+import 'package:cubitflutter/datas/database_cuaca.dart';
+import 'package:meta/meta.dart';
+
+part 'weather_state.dart';
+
+class WeatherCubit extends Cubit<WeatherState> {
+  final WeatherRepository _weatherRepository;
+
+  WeatherCubit(this._weatherRepository) : super(WeatherInitial());
+
+  Future<void> getWeather(String cityName) async {
+    try {
+      emit(WeatherLoading());
+      final weather = await _weatherRepository.fetchWeather(cityName);
+      emit(WeatherLoaded(weather));
+    } on NetworkException {
+      emit(WeatherError("Error 404"));
+    }
+  }
+}
