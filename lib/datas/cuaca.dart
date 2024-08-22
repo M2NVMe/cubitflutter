62511c1b3b0e397c645d318978c6e2c40385513e
M2NV <m2sjmail@gmail.com>
2024-08-22 22:55:27 +0700

    [BBW] FIX/ADD

--- lib/datas/cuaca.dart
+++ lib/datas/cuaca.dart
@@ -2,5 +2,6 @@
 class Cuaca {
   final String kota;
   final double celcius;
+
   Cuaca({required this.kota, required this.celcius});
 }
--- lib/utama.dart
+++ lib/utama.dart
@@ -1,20 +1,79 @@
+import 'package:cubitflutter/cubit/weather_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:cubitflutter/datas/cuaca.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class utama extends StatefulWidget {
-  const utama({super.key});
+class WeatherSearchPage extends StatelessWidget {
+  const WeatherSearchPage({super.key});
 
-  @override
-  State<utama> createState() => _utamaState();
-}
-
-class _utamaState extends State<utama> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cubit State Management app"),
+        title: Text("Weather Search"),
       ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        alignment: Alignment.center,
+        // TODO: Implement with cubit
+        child: buildInitialInput(),
+      ),
+    );;
+
+  }
+  Widget buildInitialInput() {
+    return Center(
+      child: CityInputField(),
+    );
+  }
+
+  Widget buildLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Column buildColumnWithData(Cuaca weather) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          weather.kota,
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Text(
+          // Display the temperature with 1 decimal place
+          "${weather.celcius.toStringAsFixed(1)} °C",
+          style: TextStyle(fontSize: 80),
+        ),
+        CityInputField(),
+      ],
     );
   }
 }
+
+class CityInputField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: TextField(
+        onSubmitted: (value) => submitCityName(context, value),
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          hintText: "Enter a city",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          suffixIcon: Icon(Icons.search),
+        ),
+      ),
+    );
+  }
+
+  void submitCityName(BuildContext context, String cityName) {
+    // TODO: Get weather for the city
+  }
+}
+
