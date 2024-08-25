c2b55d622ca616d4fa9424a745db163c840baea7
M2NV <m2sjmail@gmail.com>
2024-08-25 17:36:52 +0700

    [BBW] FIX/ADD

---
+++ lib/Aritmetika/uiaritmatik.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'AritmatikCubit.dart';
+
+class Uiaritmatik extends StatelessWidget {
+  const Uiaritmatik({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(title: Text("Kalkulator Artimatika"),),
+      body:
+      Column(
+        children: [
+
+        ],
+      )
+      ,
+    );
+  }
+}
