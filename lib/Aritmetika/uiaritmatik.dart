1a43925f40bce7be78d02507b8979aa9a7bd705c
M2NV <m2sjmail@gmail.com>
2024-08-27 12:38:21 +0700

    [BBW] FIX/ADD

--- lib/Aritmetika/uiaritmatik.dart
+++ lib/Aritmetika/uiaritmatik.dart
@@ -1,3 +1,5 @@
+import 'package:cubitflutter/Reusables/myButton.dart';
+import 'package:cubitflutter/Reusables/myTextfield.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'AritmatikCubit.dart';
@@ -8,16 +10,32 @@ class Uiaritmatik extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text("Kalkulator Artimatika"),),
+      appBar:AppBar(title: Text("Kalkulator Aritmatika"),),
       body:
       Column(
         children: [
-          Container(
-            child: Text("Test Page Aritmatika"),
-          )
+          Expanded(child: Container()),
+          myTextField(hintText: "1st Number", isObscure: false, textStyle: TextStyle(fontWeight: FontWeight.normal), rad: 10),
+          myTextField(hintText: "2nd  Number", isObscure: false, textStyle: TextStyle(fontWeight: FontWeight.normal), rad: 10),
+          SizedBox(height: 20,),
+          Row(
+            children: [
+              Expanded(child: Container()),
+              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "+" ,onPressed: () {},),
+              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "-" ,onPressed: () {},),
+              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "*" ,onPressed: () {},),
+              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "/" ,onPressed: () {},),
+              Expanded(child: Container()),
+            ],
+          ),
+          Expanded(child: Container()),
+          Text("Hasil",
+            style:
+            TextStyle(fontSize: 20),
+          ),
+          Expanded(child: Container()),
         ],
-      )
-      ,
+      ),
     );
   }
 }
