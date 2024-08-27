14cf1b5fb5687d42b86472358906a65f2d3a85e3
M2NV <m2sjmail@gmail.com>
2024-08-27 12:44:00 +0700

    [BBW] FIX/ADD

--- lib/Aritmetika/uiaritmatik.dart
+++ lib/Aritmetika/uiaritmatik.dart
@@ -2,6 +2,7 @@ import 'package:cubitflutter/Reusables/myButton.dart';
 import 'package:cubitflutter/Reusables/myTextfield.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'AritmatikCubit.dart';
 
 class Uiaritmatik extends StatelessWidget {
@@ -21,10 +22,18 @@ class Uiaritmatik extends StatelessWidget {
           Row(
             children: [
               Expanded(child: Container()),
-              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "+" ,onPressed: () {},),
-              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "-" ,onPressed: () {},),
-              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "*" ,onPressed: () {},),
-              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "/" ,onPressed: () {},),
+              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "+" ,onPressed: () {
+                context.read<Aritmatikcubit>().setoperator("+");
+              },),
+              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "-" ,onPressed: () {
+                context.read<Aritmatikcubit>().setoperator("-");
+              },),
+              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "*" ,onPressed: () {
+                context.read<Aritmatikcubit>().setoperator("*");
+              },),
+              myButton(backgroundColor: Colors.black45, textColor: Colors.white, radius: 5,elevation: 0, textButton: "/" ,onPressed: () {
+                context.read<Aritmatikcubit>().setoperator("/");
+              },),
               Expanded(child: Container()),
             ],
           ),
