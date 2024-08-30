e7a4865dcff01e6f9aa68c1e958fe7b8eae9536b
M2NV <m2sjmail@gmail.com>
2024-08-30 18:30:13 +0700

    [BBW] FIX/ADD

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,34 @@
 import 'package:cubitflutter/Utama.dart';
+import 'package:cubitflutter/bagun_datar/persegi_cubit.dart';
+import 'package:cubitflutter/bangun_ruang/cubit/bangun_ruang_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 void main() {
   runApp(
     MyApp(),
   );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     //TODO: mencoba utk implement blocprovider apalah itu disini, barangkali  perlu gituan (source:  https://createthrive.com/blog/introduction-to-cubit-101-learning-the-basics)
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Kelompok 8',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => BangunRuangCubit(),
+
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Kelompok 8',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const mainPage(),
       ),
-      home: const mainPage(),
     );
   }
 }
